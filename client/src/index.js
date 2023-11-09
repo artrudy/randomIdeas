e368aaa8eb88cd4a40b8e7b9965b4609eebe2adb
artёm rudoy <art.rudy@gmail.com>
2023-11-09 22:28:00 +0200

    class IdeaForm created , html form is now method

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,9 @@
 import "@fortawesome/fontawesome-free/css/all.css";
 import Modal from "./components/Modal";
+import IdeaForm from "./components/IdeaForm";
 import "./css/style.css";
 
 const modal = new modal();
+
+const ideaForm = new IdeaForm();
+ideaForm.render();
