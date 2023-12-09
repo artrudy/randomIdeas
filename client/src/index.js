2b319828081fd68a9ccd94f4f2c968a26b842455
artёm rudoy <art.rudy@gmail.com>
2023-12-09 23:13:44 +0200

    component created

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,13 @@
 import "@fortawesome/fontawesome-free/css/all.css";
 import Modal from "./components/Modal";
 import IdeaForm from "./components/IdeaForm";
+import IdeaList from "./components/ideaList";
 import "./css/style.css";
+import IdeaList from "./components/ideaList";
 
 const modal = new modal();
 
 const ideaForm = new IdeaForm();
 ideaForm.render();
+const IdeaList = new IdeaList();
+IdeaList.render();
